2c0a29587dad6d73f4c4a0092e924eb4749faa7e
MahmoudAbusaqer <mahmoudabusaqer1@gmail.com>
2022-01-17 23:45:28 +0200

    first commit

---
+++ src/main/java/com/projects/up/UniversityPayrollUpApplication.java
@@ -0,0 +1,13 @@
+package com.projects.up;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityPayrollUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityPayrollUpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projects/up/UniversityPayrollUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.up;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityPayrollUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
